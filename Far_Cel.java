d3b86f3070fd46fbb90fd5cbee23f8b5e399a561
MMeghana <63573802+MMeghana@users.noreply.github.com>
2020-08-27 18:58:12 +0530

    Add files via upload

---
+++ Far_Cel.java
@@ -0,0 +1,14 @@
+package Pack1;
+import java.util.*;
+public class Far_Cel
+{
+	public static void main(String[] args) 
+	{
+	  Scanner sc = new Scanner(System.in);
+	  float f,c;
+	  System.out.println("enter Farenheit temperature:");
+	  f = sc.nextFloat();
+	  c = ((f-32)*5)/9;
+	  System.out.println("Celsius = "+c);
+	}
+}
